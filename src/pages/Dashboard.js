148244d8221472fe775a4f8d90b9f4ebb1cf6ebe
Samuel <samsjkang@gmail.com>
2019-09-14 16:49:50 -0400

    added gauge meter

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import GaugeChart from 'react-gauge-chart'
+
+var isGaugeVisible = true; // boolean that will change upon user clicking section
+var spendingRatio = 0.86;
+
+function TagGauge() {
+  if (isGaugeVisible) {
+    return (
+      <div>
+        <GaugeChart id="gauge-chart2" textColor={'#000000'} nrOfLevels={20} percent={spendingRatio}/>
+      </div>
+    );
+  }
+  else {
+    return null;
+  }
+}
 
 export default function Dashboard() {
-    return <h1>Dashboard</h1>
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <TagGauge></TagGauge>
+    </div>
+  );
 };
