61c45377049424ce3719ac1bca633dd2aea6a254
Samuel <samsjkang@gmail.com>
2019-09-14 14:09:39 -0400

    login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const textStyle = {
+  color: 'black',
+  fontSize: '15px',
+  textAlign: 'center'
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={textStyle}>
+          Enter your Customer ID to analyze your spending habits.
+        </p> 
+        <Form className="LoginForm">
+          <Form.Group controlId="formCustomerId">
+            <Form.Control type="id" placeholder="Customer ID" />
+          </Form.Group>
+          <Button variant="dark" type="submit">
+            Submit
+          </Button>
+        </Form>
       </header>
     </div>
   );
