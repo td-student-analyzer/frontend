6c9e53c7e9169010a3fec94ee91f35bd7f6ca128
Cham <chamkank@gmail.com>
2019-09-14 17:55:36 -0700

    Added transaction table

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Jumbotron, Row, Col } from 'react-bootstrap';
+import { Container, Jumbotron, Row, Col, Table } from 'react-bootstrap';
 import { Doughnut } from 'react-chartjs-2';
 import GaugeChart from 'react-gauge-chart'
 
@@ -65,26 +65,26 @@ const doughnutData = {
 };
 
 function TagGauge(props) {
-  if (isGaugeVisible) {
-    return (
-      <div className="gauge-container">
-        <GaugeChart id={"gauge-chart" + ++gauge_counter} textColor={'#000000'} nrOfLevels={20} percent={props.spendingRatio}/>
-      </div>
-    );
-  }
-  else {
-    return null;
-  }
+	if (isGaugeVisible) {
+		return (
+			<div className="gauge-container">
+				<GaugeChart id={"gauge-chart" + ++gauge_counter} textColor={'#000000'} nrOfLevels={20} percent={props.spendingRatio} />
+			</div>
+		);
+	}
+	else {
+		return null;
+	}
 }
 
 /**
  * Gets the name of the tag clicked by the user in the donut chart
  */
 function getClickedTagFromDonutChart(elements) {
-  if (elements.length === 0) {
-    return;
-  }
-  var index = elements[0]._index;
+	if (elements.length === 0) {
+		return;
+	}
+	var index = elements[0]._index;
 	return tags[index].name;
 }
 
@@ -94,35 +94,52 @@ function generateTagBreakdown(elements) {
 }
 
 export default function Dashboard() {
-    return (
-        <div>
-          <Jumbotron>
-            <h2 className="centered-text">Your spending breakdown</h2>
-            <p className="centered-text">Click a region to expand.</p>
-          </Jumbotron>
-          <Container className="border-a">
-			      <Row className="border-b">
-              <div className="doughnut-container">
-                <Doughnut data={doughnutData} onElementsClick={elems => generateTagBreakdown(elems)}/>
-              </div>
-			      </Row>
-            <Row>
-              <Col className="border-r">
-                <Row className="border-b">
-                  <h2 className="centered-text">Spending Relative to Other Students</h2>
-                  <TagGauge spendingRatio={spendingRatio1}></TagGauge>
-                </Row>
-                <Row>
-                  <h2 className="centered-text">Spending Relative to Young Adults</h2>
-                  <TagGauge spendingRatio={spendingRatio2}></TagGauge>
-                </Row>
-              </Col>
-              <Col>
-                <h2 className="centered-text">List of Transactions</h2>
-              </Col>
-            </Row>
-          </Container>
-          
-        </div>
-    );
+	return (
+		<div>
+			<Jumbotron>
+				<h2 className="centered-text">Your spending breakdown</h2>
+				<p className="centered-text">Click a region to expand.</p>
+			</Jumbotron>
+			<Container className="border-a">
+				<Row className="border-b">
+					<div className="doughnut-container">
+						<Doughnut data={doughnutData} onElementsClick={elems => generateTagBreakdown(elems)} />
+					</div>
+				</Row>
+				<Row>
+					<Col className="border-r">
+						<Row className="border-b">
+							<h2 className="centered-text">Spending Relative to Other Students</h2>
+							<TagGauge spendingRatio={spendingRatio1}></TagGauge>
+						</Row>
+						<Row>
+							<h2 className="centered-text">Spending Relative to Young Adults</h2>
+							<TagGauge spendingRatio={spendingRatio2}></TagGauge>
+						</Row>
+					</Col>
+					<Col>
+						<h2 className="centered-text">List of Transactions</h2>
+						<Table striped bordered hover responsive>
+							<thead>
+								<tr>
+									<th>Date</th>
+									<th>Account</th>
+									<th>Description</th>
+									<th>Amount</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>*** VISA</td>
+									<td>Tim Hortons</td>
+									<td>$3.21</td>
+								</tr>
+							</tbody>
+						</Table>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
 };
